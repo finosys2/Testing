e6ee7ee77327c7fbf6542d4ca1bd61757d6a1659
Erum-Rehman <erumr0443@gmail.com>
2023-03-16 20:34:51 +0500

    vendors chk

--- cypress/e2e/Financial/Vendors.cy.js
+++ cypress/e2e/Financial/Vendors.cy.js
@@ -5,14 +5,17 @@ describe('Vendors', () => {
         // cy.visit('http://localhost:1750')
         Login();
         Save();
-        // Edit();
-        // Clear();
-        // View();
-        // Delete();
-        // Download();
+        Clear();
+        Edit();
+        View();
+        Delete();
+        Download();
     })
 })
 export function Save() {
+
+    // save btn kam nhi kr rha
+    // islie baqi operation chk krne k lie back ka btn use kia hai is func me
     var VendorCode = "208" + new Date().getMilliseconds() 
     cy.visit('http://40.81.28.195:1550/Financials/Setup/Vendors')
     cy.wait(2000);
@@ -43,5 +46,85 @@ export function Save() {
     cy.get('.MuiBox-root > div > .MuiFormControlLabel-root > .MuiTypography-root').click()
     cy.wait(1000);
     cy.get('.MuiButtonGroup-root > :nth-child(2) > .MuiButtonBase-root').click()
+    cy.wait(1000);
+}
+export function Clear() {
+    var VendorCode = "208" + new Date().getMilliseconds() 
+    cy.get('.MuiButtonGroup-root > .MuiButtonBase-root').click()
+    cy.wait(1000);
+    cy.get('.MuiContainer-root > .MuiFab-root').click()
+    cy.wait(1000);
+    cy.get('#VendorCode').type(VendorCode)
+    cy.wait(1000);
+    cy.get('#Vendor').type('Vendor')
+    cy.wait(1000);
+    cy.get('#mui-29').type(78002111)
+    cy.wait(1000);
+    cy.get('#mui-30').type('7893829832328')
+    cy.wait(1000);
+    cy.get(':nth-child(1) > .MuiStack-root > .MuiAutocomplete-root > .MuiFormControl-root').click()
+    cy.wait(1000);
+    cy.get('#VendProfileDesc-VendProfileDesc-option-0').click()
+    cy.wait(1000);
+    cy.get('.MuiButtonGroup-root > .MuiButtonBase-root').click()
+    cy.wait(1000);
+}
+
+export function Edit() {
+    
+    cy.get('[index="1"] > .MuiTableCell-paddingNone > div > .MuiButtonBase-root').click()
+    cy.wait(2000);
+    cy.get('#simple-menu > .MuiPaper-root > .MuiList-root > :nth-child(2)').click()
+    cy.wait(2000);
+    cy.get('#Vendor').clear()
+    cy.wait(1000);
+    cy.get('#Vendor').type('test')
+    cy.wait(1000);
+    cy.get(':nth-child(1) > .MuiStack-root > .MuiAutocomplete-root > .MuiFormControl-root').click()
+    cy.wait(1000);
+    cy.get('#VendProfileDesc-VendProfileDesc-option-1').click()
+    cy.wait(1000);
+    cy.get(':nth-child(7) > .MuiStack-root > .MuiAutocomplete-root > .MuiFormControl-root').click()
+    cy.wait(1000);
+    cy.get('#AccPurAcctCode-AccPurAcctCode-option-3').click()
+    cy.wait(1000);
+    cy.wait(1000);
+    cy.get('.MuiBox-root > div > .MuiFormControlLabel-root > .MuiTypography-root').click()
+    cy.wait(1000);
+    cy.get('.MuiButtonGroup-root > :nth-child(2) > .MuiButtonBase-root').click()
+    cy.wait(1000);
+}
+export function View() {
+    cy.get('[index="0"] > .MuiTableCell-paddingNone > div > .MuiButtonBase-root').click()
+    cy.wait(2000);
+    cy.get('#simple-menu > .MuiPaper-root > .MuiList-root > :nth-child(1)').click()
+    cy.wait(2000);
+    cy.get('.MuiButtonGroup-root > .MuiButtonBase-root').click()
+    cy.wait(1000);
+}
+
+export function Download() {
+    cy.get('.css-12re8tr > :nth-child(1) > :nth-child(1) > :nth-child(1) > .MuiButtonBase-root').click()
+    cy.wait(2000);
+    cy.get('.css-10nakn3-MuiModal-root-MuiPopover-root-MuiMenu-root > .MuiPaper-root > .MuiList-root > [tabindex="0"]').click()
+    cy.wait(2000);
+    cy.get('.css-12re8tr > :nth-child(1) > :nth-child(1) > :nth-child(1) > .MuiButtonBase-root').click()
+    cy.wait(2000);
+    cy.get('.css-10nakn3-MuiModal-root-MuiPopover-root-MuiMenu-root > .MuiPaper-root > .MuiList-root > [tabindex="-1"]').click()
+    cy.wait(2000);
+}
+
+export function Delete() {
+    cy.get('[index="1"] > .MuiTableCell-paddingNone > div > .MuiButtonBase-root').click()
+    cy.wait(2000);
+    cy.get('#simple-menu > .MuiPaper-root > .MuiList-root > :nth-child(3)').click()
+    cy.wait(2000);
+    cy.get('.MuiGrid-root > .MuiButton-contained').click()
+    cy.wait(1000);
+    cy.get('[index="0"] > .MuiTableCell-paddingNone > div > .MuiButtonBase-root').click()
+    cy.wait(2000);
+    cy.get('#simple-menu > .MuiPaper-root > .MuiList-root > :nth-child(3)').click()
+    cy.wait(2000);
+    cy.get('.MuiButton-outlined').click()
     cy.wait(1000);
 }
